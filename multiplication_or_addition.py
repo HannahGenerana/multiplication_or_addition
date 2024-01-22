6467582589155a1e951c5adb479125a285557292
HannahGenerana <hannahgenerana@gmail.com>
2024-01-22 22:24:57 +0800

    pseudocode

---
+++ multiplication_or_addition.py
@@ -0,0 +1,18 @@
+# Exercise 1: Calculate the multiplication and sum of two numbers
+# Given two integer numbers, return their product only if the product is equal to or lower than 1000. Otherwise, return their sum.
+
+# pseudocode
+
+# create a function checking whether the value is less tha or greater than 1000
+
+# multiply the two integer
+
+# add the two integer
+
+# print the product if less than or equal to 1000
+
+# print the sum if greater than 1000
+
+# first example
+
+# second example
